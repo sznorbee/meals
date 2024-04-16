54914964535323e142956cebeab63936b7471e61
Norbert Szekeres <norbert.szekeres@gmail.com>
2024-04-16 13:27:25 +0200

    add display ingredients, steps

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -12,8 +12,45 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(meal.imageUrl,
-          fit: BoxFit.cover, height: 200, width: double.infinity),
+      body: ListView(
+        padding: const EdgeInsets.all(10),
+        children: [
+          Image.network(meal.imageUrl,
+              fit: BoxFit.cover, height: 200, width: double.infinity),
+          const SizedBox(height: 10),
+          Text(
+            'Ingredients',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const SizedBox(height: 10),
+          for (final ingredient in meal.ingredients)
+            Text(ingredient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    )),
+          const SizedBox(height: 10),
+          Text(
+            'Steps',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const SizedBox(height: 10),
+          for (final step in meal.steps)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(step,
+              textAlign: TextAlign.left,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      )),
+            ),
+        ],
+      ),
     );
   }
 }
