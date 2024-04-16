02ec19838d9b2057111790cb677cf1bd187d9019
Norbert Szekeres <norbert.szekeres@gmail.com>
2024-04-16 16:27:54 +0200

    add keep settings button selected

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegan,
+  vegetarian,
+}
+
 class FiltersScreen extends StatefulWidget {
-  const FiltersScreen({super.key});
+  const FiltersScreen({super.key, required this.currentFilters});
+
+  final Map<Filter, bool> currentFilters;
 
   @override
   State<FiltersScreen> createState() => _FiltersScreenState();
@@ -12,6 +21,15 @@ class _FiltersScreenState extends State<FiltersScreen> {
   bool _isLactoseFree = false;
   bool _isVegan = false;
   bool _isVegetarian = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _isGlutenFree = widget.currentFilters[Filter.glutenFree]!;
+    _isLactoseFree = widget.currentFilters[Filter.lactoseFree]!;
+    _isVegan = widget.currentFilters[Filter.vegan]!;
+    _isVegetarian = widget.currentFilters[Filter.vegetarian]!;
+  }
 
   void _setGlutenFree(bool value) {
     setState(() {
@@ -56,61 +74,73 @@ class _FiltersScreenState extends State<FiltersScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Filters'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.save),
-            onPressed: () {
-              final selectedFilters = {
-                'gluten': _isGlutenFree,
-                'lactose': _isLactoseFree,
-                'vegan': _isVegan,
-                'vegetarian': _isVegetarian,
-              };
-              Navigator.of(context).pop(selectedFilters);
-            },
-          ),
-        ],
+        // actions: [
+        //   IconButton(
+        //     icon: const Icon(Icons.save),
+        //     onPressed: () {
+        //       final selectedFilters = {
+        //         'gluten': _isGlutenFree,
+        //         'lactose': _isLactoseFree,
+        //         'vegan': _isVegan,
+        //         'vegetarian': _isVegetarian,
+        //       };
+        //       Navigator.of(context).pop(selectedFilters);
+        //     },
+        //   ),
+        // ],
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Text('Adjust your meal selection',
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    )),
-          ),
-          Expanded(
-            child: ListView(
-              children: [
-                _buildSwitchListTile(
-                  'Gluten-free',
-                  'Only include gluten-free meals',
-                  _isGlutenFree,
-                  _setGlutenFree,
-                ),
-                _buildSwitchListTile(
-                  'Lactose-free',
-                  'Only include lactose-free meals',
-                  _isLactoseFree,
-                  _setLactoseFree,
-                ),
-                _buildSwitchListTile(
-                  'Vegan',
-                  'Only include vegan meals',
-                  _isVegan,
-                  _setVegan,
-                ),
-                _buildSwitchListTile(
-                  'Vegetarian',
-                  'Only include vegetarian meals',
-                  _isVegetarian,
-                  _setVegetarian,
-                ),
-              ],
+      body: PopScope(
+        canPop: false,
+        onPopInvoked: (bool didPop) {
+          if (didPop) return;
+          Navigator.of(context).pop({
+            Filter.glutenFree: _isGlutenFree,
+            Filter.lactoseFree: _isLactoseFree,
+            Filter.vegan: _isVegan,
+            Filter.vegetarian: _isVegetarian,
+          });
+        },
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text('Adjust your meal selection',
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      )),
             ),
-          ),
-        ],
+            Expanded(
+              child: ListView(
+                children: [
+                  _buildSwitchListTile(
+                    'Gluten-free',
+                    'Only include gluten-free meals',
+                    _isGlutenFree,
+                    _setGlutenFree,
+                  ),
+                  _buildSwitchListTile(
+                    'Lactose-free',
+                    'Only include lactose-free meals',
+                    _isLactoseFree,
+                    _setLactoseFree,
+                  ),
+                  _buildSwitchListTile(
+                    'Vegan',
+                    'Only include vegan meals',
+                    _isVegan,
+                    _setVegan,
+                  ),
+                  _buildSwitchListTile(
+                    'Vegetarian',
+                    'Only include vegetarian meals',
+                    _isVegetarian,
+                    _setVegetarian,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
@@ -15,6 +16,13 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
+
+  Map<Filter, bool> _selectedFilters = {
+    Filter.glutenFree: false,
+    Filter.lactoseFree: false,
+    Filter.vegan: false,
+    Filter.vegetarian: false,
+  };
 
   void _showInfoMessage(String message) {
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -43,19 +51,40 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
-  void _setScreen(String identifier) {
+  void _setScreen(String identifier) async {
     Navigator.of(context).pop();
     if (identifier == 'filters') {
-      Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-        return const FiltersScreen();
+      final result = await Navigator.of(context)
+          .push<Map<Filter, bool>>(MaterialPageRoute(builder: (_) {
+        return FiltersScreen(currentFilters: _selectedFilters,);
       }));
+      setState(() {
+        _selectedFilters = result ?? _selectedFilters;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    final availableMeals = dummyMeals.where((meal) {
+      if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      return true;
+    }).toList();
+
     Widget activePage = CategoriesScreen(
       onToggleFavorite: _toggleFavorite,
+      availableMeals: availableMeals,
     );
     var activePageTitle = 'Categories';
 
